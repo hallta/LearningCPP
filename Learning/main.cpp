293844063f4457e3bc328b3fd58e4f04e0fbb4dd
Trevor Hall <hallta@gmail.com>
2016-02-19 23:11:23 -0800

    Initial Commit

---
+++ Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning
+//
+//  Created by Trevor Hall on 2/19/16.
+//  Copyright © 2016 hallta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
